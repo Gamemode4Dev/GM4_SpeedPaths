6adca29e93c98b6046843f3884d2e6c35cfa94a3
SpiderRobotMan <spiderrobotman@gmail.com>
2016-07-05 11:45:02 -0400

    Fix speed effect and sprint jumping

--- src/main/java/co/gm4/GM4_SpeedPaths/SpeedPaths.java
+++ src/main/java/co/gm4/GM4_SpeedPaths/SpeedPaths.java
@@ -10,11 +10,12 @@ package co.gm4.GM4_SpeedPaths;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,21 +32,19 @@ public class SpeedPaths extends JavaPlugin implements Listener {
 
         saveDefaultConfig();
 
-        SPEED_MODIFIER = (float) getConfig().getDouble("speed-modifier", 1.5);
+        SPEED_MODIFIER = (float) getConfig().getDouble("speed-modifier", 0);
     }
 
+    @SuppressWarnings("deprecation")
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
-        //Prevent checking same block twice
-        if(e.getFrom().getBlockX() == e.getTo().getBlockX() && e.getFrom().getBlockZ() == e.getTo().getBlockZ()) return;
-
-        Block below = e.getPlayer().getLocation().getBlock().getRelative(BlockFace.DOWN);
+        Block below = e.getPlayer().getLocation().getBlock();
         if(below != null && below.getType() == Material.GRASS_PATH) {
             ON_PATH.add(e.getPlayer().getUniqueId());
-            e.getPlayer().setWalkSpeed((e.getPlayer().getWalkSpeed()*SPEED_MODIFIER));
-        } else if(ON_PATH.contains(e.getPlayer().getUniqueId())){
+            e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 30, 0));
+        } else if(e.getPlayer().isOnGround() && ON_PATH.contains(e.getPlayer().getUniqueId())){
             ON_PATH.remove(e.getPlayer().getUniqueId());
-            e.getPlayer().setWalkSpeed((e.getPlayer().getWalkSpeed()/SPEED_MODIFIER));
+            e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1, 0));
         }
     }
 }
